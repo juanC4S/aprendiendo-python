ed2961a803929f54f392f104302f648331dcd836
juancarlos <juancarlos@thinkpad.localhost>
2023-02-18 17:03:03 -0500

    añadido el 1er programa en python

---
+++ primerprograma.py
@@ -0,0 +1,18 @@
+nombre = input("¿cual es tu nombre?")
+print ("Tu nombre es: " +nombre)
+
+elmaximo=max(1,2,3,4,6,7)
+
+print (elmaximo)
+
+print ("la 1ra tarea de nate csmr xd ")
+
+print("dame 3 nros y te dare el mayor")
+nros1 = int(input("introduce tu 1er nro: "))
+nros2 = int(input("introduce tu 2do nro: "))
+nros3 = int(input("introduce tu 3er nro: "))
+
+maxi=max(nros1, nros2, nros3)
+
+print("el mnro mayor es: " ,maxi)
+
