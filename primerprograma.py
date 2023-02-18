4182ef982037e86878713e52a7e02ea5600d953c
juancarlos <juancarloscs2010@gmail.com>
2023-02-18 17:23:26 -0500

    agregado el escribir todo de nuevo con la opcion reset de git xd

--- primerprograma.py
+++ primerprograma.py
@@ -8,12 +8,12 @@ print (elmaximo)
 print ("la 1ra tarea de nate csmr xd ")
 
 print("dame 3 nros y te dare el mayor")
-nros1 = int(input("introduce tu 1er nro: "))
+nros1 = int(input("introduce tu 1er nrssso: "))
 nros2 = int(input("introduce tu 2do nro: "))
 nros3 = int(input("introduce tu 3er nro: "))
 
 maxi=max(nros1, nros2, nros3)
-
+#pribando el escribir de nuevo
 print("el mnro mayor es: " ,maxi)
 #aui comentando esta mrda
-
+#otro comentario de prueba git ctmr xd
