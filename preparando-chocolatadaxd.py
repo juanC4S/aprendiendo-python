640cf0dba10480b8a2350bb1b274514db4bc2f33
Juan Cas <juancarloscs2010@gmail.com>
2023-02-18 23:25:08 -0500

    ej1xd

---
+++ preparando-chocolatadaxd.py
@@ -0,0 +1,18 @@
+print ("me voy a la cocina")
+print ("abro la refri")
+
+hay_lechegloria= input("¿hay leche? (S/N)")
+hay_chocolatecuzco = input ("¿hay chocolaote (S/N)")
+
+if hay_lechegloria !="S" or hay_chocolatecuzco != "S":
+
+    print ("Voy al mercado")
+    if hay_lechegloria != "S":
+        print("-compro leche")
+    if hay_chocolatecuzco !="S":
+        print("-compro chocolate")
+   
+
+print ("ponemos la leche en el vaso")
+print ("poemos el choclate en el vaso")
+print ("mezclamos")
