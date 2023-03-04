f53ab47b1eafa1223b977da57fd6bb0179e85649
Juan Cas <juancarloscs2010@gmail.com>
2023-03-03 19:43:06 -0500

    agregue 2 archivos py sobre los ciclos for

---
+++ tabla.py
@@ -0,0 +1,10 @@
+i=range(1, 12+1)
+d=range(1, 12+1)
+
+for izq in d:
+    print("\n")
+    for der in i:
+        by=izq*der
+        print ("{} X {} = {}" .format(izq,der,by)) 
+
+print("me salio csmr!! xd")
