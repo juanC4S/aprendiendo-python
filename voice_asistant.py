13daae278578e3a30b7b408b5643e88098e6a16e
Juan Cas <juancarloscs2010@gmail.com>
2023-08-20 18:29:38 -0500

    version resumida con funciones del voicerecogg

--- voice_asistant.py
+++ voice_asistant.py
@@ -3,6 +3,20 @@ import pyttsx3
 import pyaudio
 import speech_recognition as sr
 
+def initialize_engine():
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 120)
+    engine.setProperty("voice", "spanish")
+
+    return engine
+
+def recognize_voice(r):
+    with sr.Microphone() as source:
+        print("puedes hablar")
+        audio = r.listen(source)
+        text = r.recognize_google(audio, language="es-ES")
+    return text
+
 def identify_name(text):
     name = None
     patterns = ["me llamo ([A-Za-z]+)", "mi nombre es ([A-Za-z]+)", "^([A-Za-z]+)$"]
@@ -13,25 +27,21 @@ def identify_name(text):
             print("No me ha dicho su nombre....")
     return name
 def main():
-    engine = pyttsx3.init()
-    engine.setProperty("rate", 120)
-    engine.setProperty("voice", "spanish")
+    
+    engine = initialize_engine()
 
     engine.say("Hola, Como te llamas")
     engine.runAndWait()
 
     r = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("puedes hablar")
-        audio = r.listen(source)
-        text = r.recognize_google(audio, language="es-ES")
-        name = identify_name(text)
-        if name:
-            engine.say("Encantado de conocerte, {}".format(name))
-        else:
-            engine.say("Pues mira, la verdad que no te entiendo ni mrda, puedes quitarte la chala de la boca?")
-        engine.runAndWait()
+    text = recognize_voice(r)
+    name = identify_name(text)
+    if name:
+        engine.say("Encantado de conocerte, {}".format(name))
+    else:
+        engine.say("Pues mira, la verdad que no te entiendo ni mrda, puedes quitarte la chala de la boca?")
+    engine.runAndWait()
 
 if __name__ == "__main__":
     main()
